6ea6222f39e1c5074aaffa586032bc7e4e72df7a
Yitbarek Tesfaye <yitbarektesfaye22@gmail.com>
2026-04-27 21:32:52 +0300

    style: add book spine design

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+const books = document.querySelectorAll(".book");
+const addBtn = document.getElementById("addBtn");
+
+
+const colors = [
+  "#1b2a41",
+  "#3d2c5a",
+  "#1f4d3a",
+  "#1e5f6f",
+  "#6b2d2d",
+  "#3c3c3c",
+  "#b08d57",
+  "#2a2a72",
+  "#4b5d6b",
+  "#aab7c4"
+];
+
+books.forEach((book) => {
+   book.style.background = colors[Math.floor(Math.random() * colors.length)];
+})
 
